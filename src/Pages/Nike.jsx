740efc94cf0a5d9f69ebb7a7f4b754b84d2d4aba
Your Name <you@example.com>
2024-06-27 22:26:29 +0300

    SHOES

---
+++ src/Pages/Nike.jsx
@@ -0,0 +1,21 @@
+
+const Nike = () => {
+  return (
+    <div className=" mb-24">
+      <div className="flex justify-center">
+        <h1 className="text-[40px] font-bold">Nike</h1>
+      </div>
+      <div className="flex flex-col ml-4 max-w-screen-2xl">
+        <h1 className="font-md text-2x">TOP Nike Shoes</h1>
+        <div className="w-[200px] h-[200px] ">
+        <div className="">
+          <img className="w-[200px] h-[200px] object-contain" 
+          src="../../public/nike/nike (1).jpg" alt="" />
+          </div>        
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nike
